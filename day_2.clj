5a50ef0a05f72c997cce76a1ae8a7e9653d17d3b
Mandimby RAVELOARINJAKA <mandimby.raveloarinjaka@gmail.com>
2021-12-03 17:59:53 -0500

    upload second day

---
+++ day_2.clj
@@ -0,0 +1,16 @@
+(ns com.mrave.advent-of-code-2021.day-2
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(def input (line-seq (io/reader "resources\\day_2_input.txt")))
+
+
+(def sample-input
+"forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2")
+
+
