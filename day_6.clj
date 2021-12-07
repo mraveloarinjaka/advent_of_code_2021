c0c2a21c341c904ae3819d0548e5d2a302c09518
Mandimby RAVELOARINJAKA <mandimby.raveloarinjaka@gmail.com>
2021-12-07 12:16:17 -0500

    day 6 starting

---
+++ day_6.clj
@@ -0,0 +1,6 @@
+(ns com.mrave.advent-of-code-2021.day-6
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def sample-input "3,4,3,1,2")
+(def input (slurp "resources\\day_6_input.txt"))
